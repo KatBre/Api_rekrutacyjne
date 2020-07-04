43f7f7eb1b44c01c5a5feb09fad74dad64add123
Katarzyna Brewinska <brewinska.katarzyna@gmail.com>
2020-07-04 13:40:02 +0200

    Loading data done http request done

--- src/main/java/kb/apirekrutacyjne/Main.java
+++ src/main/java/kb/apirekrutacyjne/Main.java
@@ -15,17 +15,47 @@ Dla uproszczenia zadania można przyjąć, że obecnie serwis będzie sprawdzany
  */
         Scanner scanner = new Scanner(System.in);
         NBPApiParameters parameters = new NBPApiParameters();
+        NBPApi api = new NBPApi();
 
         loadAndSetCurrency(scanner, parameters);
+        loadAndSetEndDate(scanner, parameters);
+        loadAndSetStartDate(scanner, parameters);
+        api.requestBidAskRates(parameters);
+
+//
+    }
+
+    private static void loadAndSetEndDate(Scanner scanner, NBPApiParameters parameters) {
+        do {
+            System.out.println("Please enter end date [yyyy-MM-dd]:");
+            try {
+                parameters.setEndDate(scanner.nextLine());
+            } catch (DateTimeParsingException e) {
+                System.err.println("Wrong date: " + e.getMessage());
+            }
+        } while (parameters.getEndDate() == null);
+    }
+
+    private static void loadAndSetStartDate(Scanner scanner, NBPApiParameters parameters) {
+        do {
+            System.out.println("Please enter start date [yyyy-MM-dd]:");
+            try {
+                parameters.setStartDate(scanner.nextLine());
+            } catch (DateTimeParsingException e) {
+                System.err.println("Wrong date: " + e.getMessage());
+            }
+        } while (parameters.getStartDate() == null);
     }
 
     private static void loadAndSetCurrency(Scanner scanner, NBPApiParameters parameters) {
         do {
-            System.out.println("Please enter currency [dolar, euro, rubel]:");
-            Optional<NPBCurrency> optionalCurrency = NPBCurrency.parse(scanner.nextLine());
+            System.out.println("Please enter currency [dolar,euro,rubel]:");
+            Optional<NBPCurrency> optionalCurrency = NBPCurrency.parse(scanner.nextLine());
             if (optionalCurrency.isPresent()) {
                 parameters.setCurrency(optionalCurrency.get());
+            } else {
+                System.err.println("Error: unrecognized currency.");
             }
-        } while (parameters.getCurrency() == null);
+        } while (parameters.getCurrency() == null); // wykonuj pętle, dopóki currency == null
     }
 }
